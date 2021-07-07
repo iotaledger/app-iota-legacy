a9f4b272b24199427dcb2c35e0aaa40964dc875e
shufps <shufps80@gmail.com>
2021-07-07 19:06:10 +0200

    added docker files

---
+++ docker/build_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPT_DIR=$(dirname "$0")
+if [ $SCRIPT_DIR == "./docker" ]; then
+    DOCKERFILE_PATH="docker/Dockerfile"
+    CONTEXT_PATH="."
+else
+    DOCKERFILE_PATH="Dockerfile"
+    CONTEXT_PATH=".."
+fi
+docker build -t build-app-legacy $CONTEXT_PATH -f $DOCKERFILE_PATH
